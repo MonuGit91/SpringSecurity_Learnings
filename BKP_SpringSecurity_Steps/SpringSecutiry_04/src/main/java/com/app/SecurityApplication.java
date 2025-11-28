8c3fb7057096eb099bed830d2428c8495f16cb39
MonuGit91 <monuc20082000@gmail.com>
2025-11-28 09:16:22 +0530

    Feature: Enabling H2 Database

---
+++ BKP_SpringSecurity_Steps/SpringSecutiry_04/src/main/java/com/app/SecurityApplication.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Role Based Authorization Line By Line | Spring Security Full Course Series | Video #6
+ */
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
